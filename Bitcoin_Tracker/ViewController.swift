a965c8b6f44879d3beed8c7049f08938ec067f09
mcsaatchidevelopers <tools@mcsaatchi-milano.com>
2019-09-18 15:48:49 +0200

    up

--- Bitcoin_Tracker/ViewController.swift
+++ Bitcoin_Tracker/ViewController.swift
@@ -7,14 +7,84 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+ 
+    let baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    var finalURL = ""
+    
+
+    @IBOutlet weak var currentBitPrice: UILabel!
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        currencyPicker.delegate = self
+        currencyPicker.dataSource = self
+        
     }
-
+    
+    
+    // MARK: - DELEGATES - PICKER
+    /**********************************************************************************************/
+    
+    // determines how many columns we want in out picker
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // determines the number of rows in our picker
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return currencyArray.count
+    }
+    
+    // give title names to our picker rows
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return currencyArray[row]
+    }
+    
+    // tells the picker what to do when the user selects a particular row
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        finalURL = baseURL + currencyArray[row]
+        getCurencyValues(url: finalURL)
+        
+    }
+    
+    
+    
+    // MARK: - ALAMOFIRE - API CALL
+    /**********************************************************************************************/
+    
+    func getCurencyValues(url: String) {
+        Alamofire.request(url, method: .get).responseJSON{
+            response in
+            if response.result.isSuccess {
+                print("Success")
+                print(response)
+                
+                let currencyJSON: JSON = JSON(response.result.value!)
+                self.updateCoinCurrency(json: currencyJSON)
+             
+            } else {
+                print("Error: \(String(describing: response.result.error))")
+                self.currentBitPrice.text = "Connection Issues?"
+            }
+        }
+    }
+    
+    
+    
+    // MARK: - SWIFTY JSON - PARESE JSON
+    /**********************************************************************************************/
+    func updateCoinCurrency (json: JSON) {
+            self.currentBitPrice.text = json["ask"].stringValue
+    }
+    
 
 }
 
---
+++ Podfile
@@ -0,0 +1,14 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '9.0'
+
+target 'Bitcoin_Tracker' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Bitcoin_Tracker
+
+  pod 'Alamofire'
+  pod 'SwiftyJSON'
+
+
+end
